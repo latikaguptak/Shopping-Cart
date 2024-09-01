5f4e709702aad7ec4735ce59d549f91ae3e7ef6f
latikaguptak <08guptal@gmail.com>
2024-09-01 19:00:43 +0530

    Navbar updated

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Navbar = () => {
+  const items = useSelector((state) => state.cart);
+
   return (
-    <nav className="bg-gray-800 p-4">
+    <nav className="bg-gray-800 p-4 fixed w-full z-10 top-0 left-0">
       <div className="container mx-auto flex justify-between items-center">
         <span className="text-white text-2xl font-bold">Motion Store</span>
         <div className="flex space-x-4">
@@ -21,7 +24,7 @@ const Navbar = () => {
             Cart
           </NavLink>
           <span className="text-gray-300 px-3 py-2 rounded-md text-sm font-medium">
-            Cart Items: 0
+            Cart Items: {items.length}
           </span>
         </div>
       </div>
