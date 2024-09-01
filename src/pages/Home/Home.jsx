40a18b3a6e66f5382dbe310aabe9b807d8a36e92
latikaguptak <08guptal@gmail.com>
2024-09-01 14:41:39 +0530

    Home page updated

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import Products from '../../assets/components/Products'
 
 const Home = () => {
   return (
-    <div>home</div>
+    <div className='home'>
+        <h2 className='heading'>Welcome to the Motion store</h2>
+        <section>
+            <h3>Products</h3>
+            <Products />
+        </section>
+        </div>
   )
 }
 
