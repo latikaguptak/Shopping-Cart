c8b2aba9e014697a55779051b44bbf970064991f
latikaguptak <08guptal@gmail.com>
2024-09-01 14:39:28 +0530

    css attached

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import { Outlet } from "react-router"
+import { Outlet } from "react-router";
 import Navbar from './assets/components/Navbar';
-
 
 const App = () => {
   return (
-    <div className='App'>
-      <Navbar/>
-      <Outlet/>
-      
+    <div className='App flex flex-col min-h-screen'>
+      <Navbar />
+      <main className='flex-grow'>
+        <Outlet />
+      </main>
+      <footer className='bg-gray-800 text-white text-center py-4'>
+        &copy; 2024 My Shopping Website
+      </footer>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
